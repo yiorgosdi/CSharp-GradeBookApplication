6e1856c0fba1fec4fd156d55870c15733a421985
yiorgosdi <ydimitriades@gmail.com>
2019-11-22 20:28:10 +0200

    Completed module

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -9,10 +9,11 @@ using Newtonsoft.Json.Linq;
 
 namespace GradeBook.GradeBooks
 {
-    public class BaseGradeBook
+    public class BaseGradeBook 
     {
         public string Name { get; set; }
         public List<Student> Students { get; set; }
+        public GradeBookType Type { get; set; } 
 
         public BaseGradeBook(string name)
         {
---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,14 @@
+﻿using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook 
+    {
+        public StandardGradeBook(string name) : base(name) 
+        {
+            Type = GradeBookType.Standard; 
+        }
+
+
+    }
+}
