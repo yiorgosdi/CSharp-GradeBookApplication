4288d28773d6b404e99111e93d421189e5ebcf74
yiorgosdi <ydimitriades@gmail.com>
2019-11-24 18:04:31 +0200

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -31,7 +31,8 @@ namespace GradeBook.GradeBooks
                 return 'C';
             else if (grades[(threshold * 4) - 1] <= averageGrade)
                 return 'D';
-            else return 'F'; 
+            else 
+                return 'F'; 
                        
            // return base.GetLetterGrade(averageGrade);
         }
